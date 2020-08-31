89398ea264dd944afe8d22b6ecb642ea58e6db3e
nilsbjorndahl02 <nils.bjorndahl@elev.ga.ntig.se>
2020-08-31 21:08:00 +0200

    Age Läxa
    Detta är läxan för tisdag vecka 36

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,41 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Niklas");
+
+            
+            Console.WriteLine("Skriv ditt namn:");
+
+            
+            string userName = Console.ReadLine();
+
+
+            Console.WriteLine("Du heter:" + userName);
+
+        
+            Console.WriteLine("Skriv din ålder:");
+
+
+            int age = Convert.ToInt32(Console.ReadLine());
+
+           
+            Console.WriteLine("Din ålder är: " + age);
+
+            Console.WriteLine("Lever du: y/n");
+            var answer = Console.ReadLine();
+            if (answer == "y")
+            {
+                Console.WriteLine("Härligt att du lever");
+            }
+            else
+            {
+                Console.WriteLine("Tråkigt att du är död..Take the L");
+            }
+
+
+
+
+
+            Console.WriteLine("");
         }
     }
 }
