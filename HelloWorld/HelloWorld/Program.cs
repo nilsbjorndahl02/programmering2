fae8cef075f5c055f5b62d3ff92881c2f600cd33
nilsbjorndahl02 <nils.bjorndahl@elev.ga.ntig.se>
2020-09-02 19:44:53 +0200

    Loop läxa
    Du måste välja nej på if-satsen om du vill ha resten av looparna.

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,41 +6,65 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-
-            
-            Console.WriteLine("Skriv ditt namn:");
-
-            
-            string userName = Console.ReadLine();
-
-
-            Console.WriteLine("Du heter:" + userName);
-
-        
-            Console.WriteLine("Skriv din ålder:");
-
-
-            int age = Convert.ToInt32(Console.ReadLine());
-
-           
-            Console.WriteLine("Din ålder är: " + age);
-
-            Console.WriteLine("Lever du: y/n");
-            var answer = Console.ReadLine();
-            if (answer == "y")
+            while (true)
             {
-                Console.WriteLine("Härligt att du lever");
+
+
+                Console.WriteLine("Skriv ditt namn:");
+
+
+                string userName = Console.ReadLine();
+
+
+                Console.WriteLine("Du heter:" + userName);
+
+
+                Console.WriteLine("Skriv din ålder:");
+
+
+                int age = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine("Din ålder är: " + age);
+
+                Console.WriteLine("Lever du: y/n");
+                var answer = Console.ReadLine();
+
+                if (answer == "y")
+                {
+                    Console.WriteLine("Härligt att du lever");
+                }
+                else
+                {
+                    Console.WriteLine("Tråkigt att du är död..Take the L");
+
+
+
+                    
+                    
+                    Console.WriteLine("Vänta tills den har räknat ner till noll");
+                    for (int i = 5; i > -1; i--)
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                    Console.WriteLine("Nu får du välja, skriv nirvana eller återfödelse");
+                    var end = Console.ReadLine();
+                    switch (end)
+                    {
+                        case"nirvana":
+                            Console.WriteLine("Du valde himmeln välkommen");
+                            break;
+                        case"återfödelse":
+                            Console.WriteLine("Du valde helvetet grattis");
+                            break;
+                        default:
+                            Console.WriteLine("Du valde inget synd för dig :/");
+                            break;
+                    }
+                    break;
+                }
             }
-            else
-            {
-                Console.WriteLine("Tråkigt att du är död..Take the L");
-            }
-
-
-
-
-
-            Console.WriteLine("");
         }
     }
 }
